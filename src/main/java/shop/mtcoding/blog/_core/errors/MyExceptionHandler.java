3fca8bcf387b6443458a8c744eb4b4dbba559b7d
kjs <zeeq125@gmail.com>
2024-05-21 11:52:18 +0900

    환경변수 설정

--- src/main/java/shop/mtcoding/blog/_core/errors/MyExceptionHandler.java
+++ src/main/java/shop/mtcoding/blog/_core/errors/MyExceptionHandler.java
@@ -15,38 +15,43 @@ public class MyExceptionHandler {
 
     @ExceptionHandler(Exception400.class)
     public ResponseEntity<?> ex400(Exception400 e){
+        log.warn(e.getMessage());
         ApiUtil<?> apiUtil = new ApiUtil<>(400, e.getMessage()); // http body -> 구성한 객체
         return new ResponseEntity<>(apiUtil, HttpStatus.BAD_REQUEST); // http body, http header
     }
 
     @ExceptionHandler(Exception401.class)
     public ResponseEntity<?> ex401(Exception401 e){
+        log.warn(e.getMessage());
         ApiUtil<?> apiUtil = new ApiUtil<>(401, e.getMessage());
         return new ResponseEntity<>(apiUtil, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Exception403.class)
     public ResponseEntity<?> ex403(Exception403 e){
+        log.warn(e.getMessage());
         ApiUtil<?> apiUtil = new ApiUtil<>(403, e.getMessage());
         return new ResponseEntity<>(apiUtil, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(Exception404.class)
     public ResponseEntity<?> ex404(Exception404 e){
+        log.error(e.getMessage());
         ApiUtil<?> apiUtil = new ApiUtil<>(404, e.getMessage());
         return new ResponseEntity<>(apiUtil, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception500.class)
     public ResponseEntity<?> ex500(Exception500 e){
+        log.error(e.getMessage());
         ApiUtil<?> apiUtil = new ApiUtil<>(500, e.getMessage());
         return new ResponseEntity<>(apiUtil, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> unknown(Exception e){
-        ApiUtil<?> apiUtil = new ApiUtil<>(500, "오류 : 관리자에게 문의하세요");
         log.error(e.getMessage());
+        ApiUtil<?> apiUtil = new ApiUtil<>(500, "오류 : 관리자에게 문의하세요");
         return new ResponseEntity<>(apiUtil, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
